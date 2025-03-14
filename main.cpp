e44e26ffa7c3b60448dde1b45165459d87a0a382
joinli22 <81401940+joinli22@users.noreply.github.com>
2025-03-14 22:55:03 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,51 +1,39 @@
 #include "tou.h"
 #include <gpiod.h>
-#include <iostream>
 #include <thread>
 
-using namespace std;
-
-int main() {
+int main() 
+{
     gpiod_chip *chip = gpiod_chip_open_by_name("gpiochip0");
-    if (!chip) {
-        cerr << 'Failed to open GPIO chip!' << endl;
-        return -1;
-    }
-    
+    if (!chip) return -1;
 
     gpiod_line *shock = gpiod_chip_get_line(chip, 17);
-    gpiod_line *red = gpiod_chip_get_line(chip, 27);
+    gpiod_line *red   = gpiod_chip_get_line(chip, 27);
     gpiod_line *green = gpiod_chip_get_line(chip, 18);
-    
-    if (!shock || !red || !green) {
-        cerr << "Failed to acquire GPIO line!" << endl;
+
+
+    if (!shock || !red || !green) 
+    {
         gpiod_chip_close(chip);
         return -1;
     }
-    
-    if (gpiod_line_request_both_edges_events(shock, "vibration_sensor") < 0) {
-        cerr << "Request for vibration sensor GPIO event failed!" << endl;
+
+    if (gpiod_line_request_both_edges_events(shock, "vibration_sensor") < 0) 
+    {
         gpiod_chip_close(chip);
         return -1;
     }
-    
-    if (gpiod_line_request_output(red, "led_red", 0) < 0) {
-        cerr << "Request for red LED GPIO output failed!" << endl;
+
+    if (gpiod_line_request_output(red, "led_red", 0) < 0 || gpiod_line_request_output(green, "led_green", 1) < 0) 
+    {
         gpiod_chip_close(chip);
         return -1;
     }
-    if (gpiod_line_request_output(green, "led_green", 1) < 0) {
-        cerr << "Request for green LED GPIO output failed!" << endl;
-        gpiod_chip_close(chip);
-        return -1;
-    }
-    
-    cout << "System initialization complete, waiting for vibration sensor event..." << endl;
-    
-    std::thread sensor_thread(sensor_monitor_thread, shock, [=]() {
+
+    std::thread sensor_thread(sensor_monitor_thread, shock, [=]() 
+    {
         led_control_callback(red, green);
     });
-    
     sensor_thread.join();
 
     gpiod_line_release(shock);
