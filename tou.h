388cacfce2c2cebaf32ebb9cc3a8f95d9c1e289c
DRER24 <3023903r@student.gla.ac.uk>
2025-03-15 02:25:28 +0000

    Update tou.h

--- tou.h
+++ tou.h
@@ -1,11 +1,17 @@
-#ifndef tou_H
-#define tou_H
+#ifndef TOU_H
+#define TOU_H
 
 #include <gpiod.h>
 #include <functional>
+#include "radar_sensor.h"
+#include "pcl_visualization.h"
+#include "fft_processor.h"
 
-
+// Function declarations for GPIO event handling
 void sensor_monitor_thread(gpiod_line *sensor_line, const std::function<void(void)> &callback);
 void led_control_callback(gpiod_line *led_red_line, gpiod_line *led_green_line);
 
-#endif // SENSOR_H
+// Function declarations for radar processing
+void process_radar_data(RadarSensor &radar, PCLVisualization &pclViz, FFTProcessor &fftProc);
+
+#endif // TOU_H
